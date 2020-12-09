26ff467fe25a5d2f36acf15b26090a1ff08f5f3d
cookcodeblog <cookcode_me@sina.com>
2020-12-09 15:01:07 +0800

    Initial commit

---
+++ src/main/java/com/example/store/StorePreferenceApplication.java
@@ -0,0 +1,20 @@
+package com.example.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class StorePreferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorePreferenceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+}
---
+++ src/main/java/com/example/store/controller/PreferenceController.java
@@ -0,0 +1,51 @@
+package com.example.store.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class PreferenceController {
+
+    private static final String RESPONSE_STRING_FORMAT = "preference => %s\n";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${recommendations.api.url}")
+    private String remoteURL;
+
+    @RequestMapping("/")
+    public ResponseEntity<?> getPreferences() {
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
+            String response = responseEntity.getBody();
+            return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
+        } catch (HttpStatusCodeException ex) {
+            log.warn("Exception trying to get the response from recommendation service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT,
+                            String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
+        } catch (RestClientException ex) {
+            log.warn("Exception trying to get the response from recommendation service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT, ex.getMessage()));
+        }
+    }
+
+    private String createHttpErrorResponseString(HttpStatusCodeException ex) {
+        String responseBody = ex.getResponseBodyAsString().trim();
+        if (responseBody.startsWith("null")) {
+            return ex.getStatusCode().getReasonPhrase();
+        }
+        return responseBody;
+    }
+}
